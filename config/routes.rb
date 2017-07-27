23ff54488371a3e1ec5921a99248f0c7768e44af
Daniel Moreto <dfmoreto@gmail.com>
2017-07-26 22:54:45 -0300

    Sidekiq configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   devise_for :users
+  mount Sidekiq::Web => 'sidekiq'
 
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
