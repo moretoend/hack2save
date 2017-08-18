f7b7f314dc2c0ce82fe416380cba99471ef4a9bd
Daniel Moreto <dfmoreto@gmail.com>
2017-08-18 00:35:32 -0300

    Enum for profile was added on Permission model

--- test/models/permission_test.rb
+++ test/models/permission_test.rb
@@ -5,4 +5,5 @@ class PermissionTest < ActiveSupport::TestCase
   should belong_to :institution
 
   should validate_presence_of :profile 
+  should define_enum_for(:profile).with([:owner, :admin, :approver])
 end
