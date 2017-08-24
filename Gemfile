d98051d2bb6c06326fa5f845e3785bea557d2489
Saulo Mendonça <saulomendonca@gmail.com>
2017-08-23 23:07:20 -0300

    add cancancan to manage authorization

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'redis'
 gem 'sidekiq'
 gem 'simple_form'
 gem 'dotenv-rails'
+gem 'cancancan', '~> 2.0'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  load_and_authorize_resource
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to '/', :alert => I18n.t('messages.authorization')
+  end
 end
