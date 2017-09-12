d1c07c2fdff5d6468601c30448661d8de9a2055a
Saulo Mendonça <saulomendonca@gmail.com>
2017-09-12 19:27:56 -0300

    add subscriptions association to institution

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -2,6 +2,7 @@ class Institution < ApplicationRecord
   has_many :jobs, dependent: :destroy
   has_many :permissions, dependent: :destroy
   has_many :users, through: :permissions
+  has_many :subscriptions, through: :jobs
 
   validates :name, presence: true
   validates :description, presence: true
