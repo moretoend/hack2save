ec4d5c140a42587b183f7facbae9420591392679
Daniel Moreto <dfmoreto@gmail.com>
2017-09-17 14:53:28 -0300

    TestHelper was updated to require support folder and to include Integration Tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,13 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+Dir[Rails.root.join("test/support/**/*.rb")].each { |f| require f }
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include I18nSupport
+end
 
-  # Add more helper methods to be used by all tests here...
+
+class ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
 end
